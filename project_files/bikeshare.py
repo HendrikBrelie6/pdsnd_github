4395b03626ee875c299d4b4d36c9e39b20ea5686
Hendrik von der Brelie <Hendrik.Brelie@gmail.com>
2023-05-06 16:56:03 -0400

    Simplified code in load_data function to extract start time column only once instead of twice

--- project_files/bikeshare.py
+++ project_files/bikeshare.py
@@ -71,7 +71,7 @@ def load_data(city, month, day):
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
-    df['day'] = df['Start Time'].dt.weekday_name
+    df['day'] = df['Start Time'].dt.strftime('%A')
     # df['day'] = pd.to_datetime(df['day'], format='%A').dt.strftime('%a')
     df['hour'] = df['Start Time'].dt.hour
 
